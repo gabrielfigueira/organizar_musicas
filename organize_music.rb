9b58699b9a33a9aa6804c62f97eb5beb45ef3617
Gabriel Figueira <figueira.gabriel11@gmail.com>
2016-06-05 23:50:56 -0300

    Correção

--- organize_music.rb
+++ organize_music.rb
@@ -5,17 +5,8 @@ my_dir.each do |music_name|
 	find_band_name = music_name.split("-")
 	band_name = find_band_name[0]
 	dir_destiny = "#{band_name}/"
-
-	Dir.mkdir(band_name)unless File.exists?(band_name)
+	Dir.mkdir(band_name)
+	Dir.mkdir(band_name) unless File.exists?(band_name)
 	FileUtils.mv music_name, dir_destiny
-	# unless File.directory?(band_name)
-	# 	p "existe diretorio"
-	# else
-	# 	p "Não existe diretorio"
-	# 	FileUtils.mkdir(band_name)
-	# 	dir_destiny = "#{band_name}/"
-	# 	
-	# end
-
 end
 
