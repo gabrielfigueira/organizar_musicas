cb07470da05c81fe3714aa4386c5da9e4c6f67da
Gabriel Figueira <figueira.gabriel11@gmail.com>
2016-06-06 08:45:05 -0300

    Correçao

--- organize_music.rb
+++ organize_music.rb
@@ -5,7 +5,6 @@ my_dir.each do |music_name|
 	find_band_name = music_name.split("-")
 	band_name = find_band_name[0]
 	dir_destiny = "#{band_name}/"
-	Dir.mkdir(band_name)
 	Dir.mkdir(band_name) unless File.exists?(band_name)
 	FileUtils.mv music_name, dir_destiny
 end
